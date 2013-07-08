e91345f376e8423a016dd8872575d6922eff8151
Christopher Brown <io@henrian.com>
2013-07-08 18:56:20 -0500

    Indentation fixed

--- v2/python/search.py
+++ v2/python/search.py
@@ -35,81 +35,81 @@ options, args = parser.parse_args()
 
 # Required options
 if not options.consumer_key:
-  parser.error('--consumer_key required')
+    parser.error('--consumer_key required')
 if not options.consumer_secret:
-  parser.error('--consumer_secret required')
+    parser.error('--consumer_secret required')
 if not options.token:
-  parser.error('--token required')
+    parser.error('--token required')
 if not options.token_secret:
-  parser.error('--token_secret required')
+    parser.error('--token_secret required')
 
 if not options.location and not options.bounds and not options.point:
-  parser.error('--location, --bounds, or --point required')
+    parser.error('--location, --bounds, or --point required')
 
 # Setup URL params from options
 url_params = {}
 if options.term:
-  url_params['term'] = options.term
+    url_params['term'] = options.term
 if options.location:
-  url_params['location'] = options.location
+    url_params['location'] = options.location
 if options.bounds:
-  url_params['bounds'] = options.bounds
+    url_params['bounds'] = options.bounds
 if options.point:
-  url_params['ll'] = options.point
+    url_params['ll'] = options.point
 if options.offset:
-  url_params['offset'] = options.offset
+    url_params['offset'] = options.offset
 if options.limit:
-  url_params['limit'] = options.limit
+    url_params['limit'] = options.limit
 if options.cc:
-  url_params['cc'] = options.cc
+    url_params['cc'] = options.cc
 if options.lang:
-  url_params['lang'] = options.lang
+    url_params['lang'] = options.lang
 if options.current_location:
-  url_params['cll'] = options.current_location
+    url_params['cll'] = options.current_location
 if options.radius:
-  url_params['radius_filter'] = options.radius
+    url_params['radius_filter'] = options.radius
 if options.category:
-  url_params['category_filter'] = options.category
+    url_params['category_filter'] = options.category
 if options.deals:
-  url_params['deals_filter'] = options.deals
+    url_params['deals_filter'] = options.deals
 if options.sort:
-  url_params['sort'] = options.sort
+    url_params['sort'] = options.sort
 
 
 def request(host, path, url_params, consumer_key, consumer_secret, token, token_secret):
-  """Returns response for API request."""
-  # Unsigned URL
-  encoded_params = ''
-  if url_params:
-    encoded_params = urllib.urlencode(url_params)
-  url = 'http://%s%s?%s' % (host, path, encoded_params)
-  print 'URL: %s' % (url,)
+    """Returns response for API request."""
+    # Unsigned URL
+    encoded_params = ''
+    if url_params:
+        encoded_params = urllib.urlencode(url_params)
+    url = 'http://%s%s?%s' % (host, path, encoded_params)
+    print 'URL: %s' % (url,)
 
-  # Sign the URL
-  consumer = oauth2.Consumer(consumer_key, consumer_secret)
-  oauth_request = oauth2.Request('GET', url, {})
-  oauth_request.update({'oauth_nonce': oauth2.generate_nonce(),
-                        'oauth_timestamp': oauth2.generate_timestamp(),
-                        'oauth_token': token,
-                        'oauth_consumer_key': consumer_key})
+    # Sign the URL
+    consumer = oauth2.Consumer(consumer_key, consumer_secret)
+    oauth_request = oauth2.Request('GET', url, {})
+    oauth_request.update({'oauth_nonce': oauth2.generate_nonce(),
+                          'oauth_timestamp': oauth2.generate_timestamp(),
+                          'oauth_token': token,
+                          'oauth_consumer_key': consumer_key})
 
-  token = oauth2.Token(token, token_secret)
-  oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
-  signed_url = oauth_request.to_url()
-  print 'Signed URL: %s\n' % (signed_url,)
+    token = oauth2.Token(token, token_secret)
+    oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+    signed_url = oauth_request.to_url()
+    print 'Signed URL: %s\n' % (signed_url,)
 
-  # Connect
-  try:
-    conn = urllib2.urlopen(signed_url, None)
+    # Connect
     try:
-      response = json.loads(conn.read())
-    finally:
-      conn.close()
-  except urllib2.HTTPError, error:
-    response = json.loads(error.read())
+        conn = urllib2.urlopen(signed_url, None)
+        try:
+            response = json.loads(conn.read())
+        finally:
+            conn.close()
+    except urllib2.HTTPError, error:
+        response = json.loads(error.read())
 
-  return response
+    return response
 
-response = request(options.host, '/v2/search', url_params, options.consumer_key, options.consumer_secret, options.token, options.token_secret)
+response = request(options.host, '/v2/search', url_params,
+    options.consumer_key, options.consumer_secret, options.token, options.token_secret)
 print json.dumps(response, sort_keys=True, indent=2)
-
