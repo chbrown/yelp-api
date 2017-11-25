f2cfed68e1ddd1017b2110d8270a68cdd066bb08
Christopher Brown <io@henrian.com>
2017-11-24 22:02:02 -0600

    Convert to Python3 with 2to3 and autopep8

--- yelp/__init__.py
+++ yelp/__init__.py
@@ -1,17 +1,18 @@
 import os
 import sys
+import urllib.parse
 import oauth2
 import requests
-import urllib
+
 
 consumer = oauth2.Consumer(os.environ['YELP_CONSUMER_KEY'], os.environ['YELP_CONSUMER_SECRET'])
 token = oauth2.Token(os.environ['YELP_TOKEN'], os.environ['YELP_TOKEN_SECRET'])
 
 
 def get(params):
-    encoded_params = urllib.urlencode(params)
+    encoded_params = urllib.parse.urlencode(params)
     url = 'http://api.yelp.com/v2/search?' + encoded_params
-    print >> sys.stderr, 'url', url
+    print('url', url, file=sys.stderr)
     oauth_request = oauth2.Request('GET', url, {})
     oauth_request.update({
         'oauth_nonce': oauth2.generate_nonce(),
@@ -20,7 +21,8 @@ def get(params):
         'oauth_consumer_key': consumer.key,
     })
 
-    oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+    oauth_request.sign_request(
+        oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
     signed_url = oauth_request.to_url()
 
     response = requests.get(signed_url)
--- yelp/business.py
+++ yelp/business.py
@@ -1,78 +1,76 @@
 import json
-import oauth2
 import argparse
-import urllib
-import urllib2
-
-parser = argparse.ArgumentParser()
-parser.add_option('-c', '--consumer_key', dest='consumer_key', help='OAuth consumer key (REQUIRED)')
-parser.add_option('-s', '--consumer_secret', dest='consumer_secret', help='OAuth consumer secret (REQUIRED)')
-parser.add_option('-t', '--token', dest='token', help='OAuth token (REQUIRED)')
-parser.add_option('-e', '--token_secret', dest='token_secret', help='OAuth token secret (REQUIRED)')
-parser.add_option('-a', '--host', dest='host', help='Host', default='api.yelp.com')
-parser.add_option('-i', '--id', dest='id', help='Business')
-parser.add_option('-u', '--cc', dest='cc', help='Country code')
-parser.add_option('-n', '--lang', dest='lang', help='Language code')
-
-
-options, args = parser.parse_args()
-
-# Required options
-if not options.consumer_key:
-  parser.error('--consumer_key required')
-if not options.consumer_secret:
-  parser.error('--consumer_secret required')
-if not options.token:
-  parser.error('--token required')
-if not options.token_secret:
-  parser.error('--token_secret required')
-
-if not options.id:
-  parser.error('--id required')
-
-url_params = {}
-if options.cc:
-  url_params['cc'] = options.cc
-if options.lang:
-  url_params['lang'] = options.lang
-
-
-path = '/v2/business/%s' % (options.id,)
+import urllib.request
+import urllib.error
+import urllib.parse
+import oauth2
 
 
 def request(host, path, url_params, consumer_key, consumer_secret, token, token_secret):
-  """Returns response for API request."""
-  # Unsigned URL
-  encoded_params = ''
-  if url_params:
-    encoded_params = urllib.urlencode(url_params)
-  url = 'http://%s%s?%s' % (host, path, encoded_params)
-  print 'URL: %s' % (url,)
+    """Returns response for API request."""
+    # Unsigned URL
+    encoded_params = ''
+    if url_params:
+        encoded_params = urllib.parse.urlencode(url_params)
+    url = 'http://%s%s?%s' % (host, path, encoded_params)
+    print('URL: %s' % (url,))
 
-  # Sign the URL
-  consumer = oauth2.Consumer(consumer_key, consumer_secret)
-  oauth_request = oauth2.Request('GET', url, {})
-  oauth_request.update({'oauth_nonce': oauth2.generate_nonce(),
-                        'oauth_timestamp': oauth2.generate_timestamp(),
-                        'oauth_token': token,
-                        'oauth_consumer_key': consumer_key})
+    # Sign the URL
+    consumer = oauth2.Consumer(consumer_key, consumer_secret)
+    oauth_request = oauth2.Request('GET', url, {})
+    oauth_request.update({'oauth_nonce': oauth2.generate_nonce(),
+                          'oauth_timestamp': oauth2.generate_timestamp(),
+                          'oauth_token': token,
+                          'oauth_consumer_key': consumer_key})
 
-  token = oauth2.Token(token, token_secret)
-  oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
-  signed_url = oauth_request.to_url()
-  print 'Signed URL: %s\n' % (signed_url,)
+    token = oauth2.Token(token, token_secret)
+    oauth_request.sign_request(
+        oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+    signed_url = oauth_request.to_url()
+    print('Signed URL: %s\n' % (signed_url,))
 
-  # Connect
-  try:
-    conn = urllib2.urlopen(signed_url, None)
+    # Connect
     try:
-      response = json.loads(conn.read())
-    finally:
-      conn.close()
-  except urllib2.HTTPError, error:
-    response = json.loads(error.read())
+        conn = urllib.request.urlopen(signed_url, None)
+        try:
+            response = json.loads(conn.read())
+        finally:
+            conn.close()
+    except urllib.error.HTTPError as error:
+        response = json.loads(error.read())
 
-  return response
+    return response
 
-response = request(options.host, path, url_params, options.consumer_key, options.consumer_secret, options.token, options.token_secret)
-print json.dumps(response, sort_keys=True, indent=2)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--consumer_key', required=True,
+                        help='OAuth consumer key')
+    parser.add_argument('-s', '--consumer_secret', required=True,
+                        help='OAuth consumer secret')
+    parser.add_argument('-t', '--token', required=True,
+                        help='OAuth token')
+    parser.add_argument('-e', '--token_secret', required=True,
+                        help='OAuth token secret')
+    parser.add_argument('-a', '--host', default='api.yelp.com',
+                        help='Host')
+    parser.add_argument('-i', '--id', required=True,
+                        help='Business')
+    parser.add_argument('-u', '--cc',
+                        help='Country code')
+    parser.add_argument('-n', '--lang',
+                        help='Language code')
+
+    opts = parser.parse_args()
+
+    url_params = {}
+    if opts.cc:
+        url_params['cc'] = opts.cc
+    if opts.lang:
+        url_params['lang'] = opts.lang
+
+    path = '/v2/business/{}'.format(opts.id)
+
+    response = request(opts.host, path, url_params, opts.consumer_key,
+                       opts.consumer_secret, opts.token, opts.token_secret)
+    print(json.dumps(response, sort_keys=True, indent=2))
